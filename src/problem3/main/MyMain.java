931792316414de52a827470321e3174ffbf233bf
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-03-28 14:04:35 +0530

    solved problem 3

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student("Rohan", 50));
+        queue.enqueue(new Student("phoebe", 65));
+        queue.enqueue(new Student("jon", 69));
+        queue.enqueue(new Student("putin", 1));
+        queue.enqueue(new Student("chandler", 10));
+        queue.enqueue(new Student("joey", 76));
+
+        queue.show();
+        queue.dequeue();
+        queue.show();
+
+    }
 }
